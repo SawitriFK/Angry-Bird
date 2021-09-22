85fd26ccdd0c46206f14c15dd7388160aed50965
SawitriFK <sawitrifinak@gmail.com>
2021-09-22 22:09:29 +0700

    First Upload

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    public BoxCollider2D TapCollider;
+
+    private Bird _shotBird;
+    private bool _isGameEnded = false;
+
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private GameObject losePanel;
+
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+        if (_isGameEnded)
+        {
+            return;
+        }
+        Birds.RemoveAt(0);
+        //Agar saat bird diganti akan terdeksi efek nya
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+
+        if (Birds.Count == 0 && Enemies.Count > 0)
+        {
+            _isGameEnded = true;
+            losePanel.SetActive(true);
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+            winPanel.SetActive(true);
+        }
+    }
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+}
---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject pauseButton;
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+
+    public void ChangeScene(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+    }
+}
